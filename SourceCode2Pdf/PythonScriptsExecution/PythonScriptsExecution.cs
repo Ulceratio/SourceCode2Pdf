5f1cde67c1df1471cfebc205ba929f0a591d6488
OneMoreCrown <nik.facer@yandex.ru>
2021-09-09 17:24:18 +0300

    Создан класс для запуска скриптов

---
+++ SourceCode2Pdf/PythonScriptsExecution/PythonScriptsExecution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+
+namespace SourceCode2Pdf.PythonScriptsExecution
+{
+    public class PythonScriptsExecution <T>
+    {
+        public T Execute(string file)
+        {
+            ScriptEngine pythonScript = Python.CreateEngine();
+            ScriptScope scope = pythonScript.CreateScope();
+            pythonScript.ExecuteFile(file);
+            return scope.GetVariable("result");// изменить имя переменной в зависимости от ее имени в скрипте
+        }
+
+    }
+}
