809050563fa2fc20925abf68e83da99e059279a8
OneMoreCrown <nik.facer@yandex.ru>
2021-09-10 21:58:27 +0300

    PythonScriptExecutor interface was added

---
+++ SourceCode2Pdf/PythonScriptsExecution/PythonScriptExecutor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SourceCode2Pdf.PythonScriptsExecution
+{
+    interface PythonScriptExecutor<T>
+    {
+        T Execute(string scriptFile);
+    }
+}
